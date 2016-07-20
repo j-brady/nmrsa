0a75ef99874fb0e0e00e8737ca6eb2b5ad3246e1
Jacob Brady <jacob.brady0449@gmail.com>
2016-07-20 13:13:15 -0400

    seaborn and ln plot

--- scripts/proc_with_yaml.py
+++ scripts/proc_with_yaml.py
@@ -8,6 +8,7 @@ import subprocess as sp
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+import seaborn as sns
 import numpy as np
 import nmrglue as ng
 from scipy.optimize import curve_fit
@@ -18,6 +19,8 @@ from nmrsa.ng import get_region, normalise, integrate
 from nmrsa.fitting import Diffusion
 from nmrsa.runNMRPipe import run_pipe_script
 
+#sns.set_style("whitegrid")
+sns.set_style("ticks")
 
 def load_yaml(yaml_file):
     """ reads files containing YAML and converts to dictionary """
@@ -64,7 +67,16 @@ def run_proc(yaml_dict,g2s,pdf,outfile):
             
             colormap = plt.cm.winter_r
             plt.rcParams['axes.color_cycle'] = [colormap(i) for i in np.linspace(0, 1, regions.shape[0])]
-            fig, axes = plt.subplots(1, 2,figsize=(12,6))
+            fig = plt.figure(figsize=(12,6))
+            ax1 = fig.add_subplot(121)
+            ax2 = fig.add_subplot(122)
+
+            #ax1 = plt.subplot2grid((4,4), (2, 0), colspan=2,rowspan=2)
+            #ax2 = plt.subplot2grid((4,4), (0, 0), colspan=2,rowspan=2)
+            #ax3 = plt.subplot2grid((4,4), (1, 0), colspan=1,rowspan=1)
+            # x,y,wid,height
+            ax3 = plt.axes([.27, .55, .20, .3])
+            axes = [ax1,ax2,ax3]
 
             fit = Diffusion(T_diff=v["T_diff"],delta=v["delta"],Dtype=v["type"],bipolar=v["bipolar"])
             popt, pcov = curve_fit(fit.func, g2s, areas,[I0,D])
@@ -90,7 +102,7 @@ def run_proc(yaml_dict,g2s,pdf,outfile):
 
 def plot_fit(axes,g2s,areas,regions,region_ppm,func,popt):
     x = np.linspace(g2s.min(),g2s.max())
-    ax1,ax2 = axes
+    ax1,ax2,ax3 = axes
     ax2.set_xlim(max(region_ppm),min(region_ppm))
     [ax2.plot(region_ppm,region,label="%d"%g2) for g2,region in zip(g2s,normalise(regions))]        
     ax2.set_xlabel("ppm")
@@ -101,7 +113,12 @@ def plot_fit(axes,g2s,areas,regions,region_ppm,func,popt):
     ax1.set_ylabel(r"I/I$_{0}$") 
     ax1.set_xlabel(r"G$^2$ $(G^2cm^{-2})$")
 
-
+    
+    ax3.plot(x,np.log(func(x,*popt)),g2s,np.log(areas),"o")
+    ax3.set_ylabel(r"ln(I/I$_{0})$") 
+    ax3.set_xlabel(r"G$^2$ $(G^2cm^{-2})$")
+    
+    sns.despine()
 
 if __name__ == "__main__":
     """ Argument parser """

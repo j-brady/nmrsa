32f472a87579d3632e1016b2da823efa51dcc50e
Jacob Brady <jacob.brady0449@gmail.com>
2016-07-30 20:09:37 -0400

    add error messages as create backup files

--- scripts/makeYamlFiles.py
+++ scripts/makeYamlFiles.py
@@ -14,6 +14,9 @@ info = ["File created by %s"%user,t]
 PATH = os.getcwd()
 DIRS = filter(os.path.isdir, os.listdir(PATH))
 DIRS = [i for i in DIRS if "ser" in os.listdir(os.path.join(PATH,i))]
+# Need to figure out how to deal with 1d data set
+if len(DIRS)<1:
+    raise Exception("No ser files found! Are you sure you downloaded the data?")
 
 """ argparser stuff """
 parser = argparse.ArgumentParser(description="Script to generate yaml files for processing diffusion data")
@@ -23,6 +26,9 @@ parser.add_argument("-p","--proc",
 parser.add_argument("-g","--gradients",
                     type=str,help="Name for yaml file containing file names and parameters for processing.",
                     default="gradients.yaml")
+parser.add_argument("-ng","--nogradients",
+                    help="Do not generate yaml file containing gradients",
+                    action="store_true")
 parser.add_argument("-d","--dtype",type=str,help="Type of diffusion experiment: single (1Q) or triple (3Q) quantum.",
                     default="1Q",choices=set(("1Q","2Q","3Q")))
 parser.add_argument("-b","--bipolar", help="Use flag if bipolar gradients are used",
@@ -43,15 +49,28 @@ bipolar = args.bipolar
 start_ppm = 2.1
 end_ppm = 1.9
 
-outname = "proc.yaml"
+outname = args.proc 
+if outname in os.listdir("./"):
+    os.system("cp %s %s.backup"%(outname,outname))
+    print("Moved %s to %s.backup"%(outname,outname))
+
 outfile = open(outname,"w")
 outfile.write(proc_temp.render(info=info,directories=DIRS,filename=filename,fid_com=fid_com,
         ft_com=ft_com,start_ppm=start_ppm,end_ppm=end_ppm,bipolar=bipolar,type=dtype))
 
 """ gradients.yaml """ 
-max_strength = 45.0
-percentages = arange(0.1,1,0.1)
-outname = "gradients.yaml"
-outfile = open(outname,"w")
-outfile.write(gradients_temp.render(info=info,max_strength=max_strength,percentages=percentages))
+no_gradients = args.nogradients
+outname = args.gradients
+if no_gradients:
+    pass
+
+else:
+    if outname in os.listdir("./"):
+        os.system("cp %s %s.backup"%(outname,outname))
+        print("Moved %s to %s.backup"%(outname,outname))
+
+    max_strength = 45.0
+    percentages = arange(0.1,1,0.1)
+    outfile = open(outname,"w")
+    outfile.write(gradients_temp.render(info=info,max_strength=max_strength,percentages=percentages))
 

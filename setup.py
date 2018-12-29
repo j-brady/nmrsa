a62b8853214ea380355c4ca0a36ec3f7a94d9a11
Jacob Brady <jacob.brady0449@gmail.com>
2018-12-28 19:47:32 -0500

    added spec.py to setup.py

--- setup.py
+++ setup.py
@@ -2,27 +2,28 @@ import sys
 from distutils.core import setup
 
 # Update file headers with running version of python
-files = ['scripts/makeYamlFiles.py','scripts/proc_with_yaml.py']
+files = ["scripts/makeYamlFiles.py", "scripts/proc_with_yaml.py", "scripts/spec.py"]
 for i in files:
 
-    with open(i,'r') as f:
+    with open(i, "r") as f:
         lines = f.readlines()
         if lines[0].startswith("#!"):
             lines.pop(0)
             f_str = "".join(lines)
-        else: 
+        else:
             f_str = "".join(lines)
 
-        s = "#!" + sys.executable + '\n' + f_str
-    with open(i,'w') as f:
+        s = "#!" + sys.executable + "\n" + f_str
+    with open(i, "w") as f:
         f.write(s)
 
 
-setup(name='nmrsa',
-      version='',
-      description='NMR processing scripts',
-      author='Jacob Brady and Rui Huang',
-      packages=['nmrsa'],
-      scripts=files,
-      package_data={"nmrsa":["templates/*"]},
-     )
+setup(
+    name="nmrsa",
+    version="",
+    description="NMR processing scripts",
+    author="Jacob Brady and Rui Huang",
+    packages=["nmrsa"],
+    scripts=files,
+    package_data={"nmrsa": ["templates/*"]},
+)
